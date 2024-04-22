5d05140e36451ebff7d9a7901dc70342d64f0df7
Daniel Rivas <drivas8891@gmail.com>
2024-04-21 17:50:58 -0700

    Handle input stream failure for istreamstream object

--- Source.cpp
+++ Source.cpp
@@ -21,8 +21,11 @@ int main() {
         cin.ignore();  // Ignore the newline character left in the input buffer
         Graph g(numVertices);
 
-        // Build the graph based on user input
-        for (int i = 0; i < numVertices; ++i) {
+        // Use a flag to break out of loop upon invalid input
+        bool invalid_input_detected = false;
+
+        // Build the graph using valid user input
+        for (int i = 0; i < numVertices && !invalid_input_detected; ++i) {
             string line;
             cout << "\nAdjacency list for vertex " << i << ": ";
 
@@ -33,13 +36,24 @@ int main() {
             while (iss >> vertex) {
                 g.addEdge(i, vertex);
             }
+
+            // Check for failure not at the end of file
+            if (iss.fail() && !iss.eof()) { // Check for failure not at the end of file
+                iss.clear();
+                iss.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+                std::cout << "\nError: Invalid Vertices Detected\n";
+                invalid_input_detected = true;
+            }
         }
 
-        int startVertex;
-        cout << "Enter the starting node for DFS: ";
-        cin >> startVertex;
-        cout << "DFS: ";
-        g.DFS(startVertex);
+        if (!invalid_input_detected) {
+            int startVertex;
+            cout << "Enter the starting node for DFS: ";
+            cin >> startVertex;
+            cout << "DFS: ";
+            g.DFS(startVertex);
+        }
     }
 
     cout << "Terminating Program...\n";
